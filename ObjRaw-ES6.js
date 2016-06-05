54fdf7693ec5a9138ad8562508b1a60975c5fd3c
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-06-05 11:14:28 +0200

    Create ObjRaw-ES6.js

---
+++ ObjRaw-ES6.js
@@ -0,0 +1,17 @@
+const call=(function(e){const o=e.prototype;return o.call.bind(o.bind);})(Function);
+//
+const RawObject=call(Object.create,null,null);
+//
+const a=RawObject();
+//
+const b=RawObject({
+	foo:{
+		writable:true,
+		configurable:true,
+		value:'hello'
+	}
+});
+//
+const dir=call(console.dir,console);
+dir(a);
+dir(b);//created with descriptor
